28c75bcc5ec192b0bd0ff583c2db73f13261bb7d
Ali Najmabadi <alinajmabadizadeh2002@gmail.com>
2024-04-22 16:01:01 +0330

    add sqlserver conf

--- Program.cs
+++ Program.cs
@@ -1,5 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
 var builder = WebApplication.CreateBuilder(args);
 
+// if (builder.Environment.IsDevelopment())
+// {
+//     builder.Services.AddDbContext<MvcMovieContext>(options =>
+//         options.UseSqlite(builder.Configuration.GetConnectionString("MvcMovieContext")));
+// }
+// else
+// {
+    builder.Services.AddDbContext<MvcMovieContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("ProductionMvcMovieContext")));
+// }
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
