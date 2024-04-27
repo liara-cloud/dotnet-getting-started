39b03f57f7dfd66546dcec63c8c2f695b31bd7f0
Ali Najmabadi <alinajmabadizadeh2002@gmail.com>
2024-04-27 14:25:35 +0330

    add websocket

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
+using SignalRChat.Hubs;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddSignalR();
+
 
 var app = builder.Build();
 
@@ -21,5 +24,6 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapRazorPages();
+app.MapHub<ChatHub>("/chatHub");
 
 app.Run();
