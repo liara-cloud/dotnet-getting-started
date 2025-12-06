28a64586f3708d38010bbe60374441fa66933091
Ali Najmabadi <alinajmabadizadeh2002@gmail.com>
2025-12-06 15:00:40 +0330

    add cc bcc

--- Program.cs
+++ Program.cs
@@ -2,28 +2,26 @@ using System.Net;
 using System.Net.Mail;
 using dotenv.net;
 
-DotEnv.Load(); // Load the environment variables from .env file
+DotEnv.Load(); 
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/send-test-email", async context =>
 {
-    // Read SMTP settings from environment variables
     var smtpHost = Environment.GetEnvironmentVariable("MAIL_HOST");
     int smtpPort = int.Parse(Environment.GetEnvironmentVariable("MAIL_PORT") ?? "587");
     var smtpUser = Environment.GetEnvironmentVariable("MAIL_USER");
     var smtpPassword = Environment.GetEnvironmentVariable("MAIL_PASSWORD");
     var fromAddress = Environment.GetEnvironmentVariable("MAIL_FROM_ADDRESS") ?? "info@example.com";
-    var toAddress = "recipient@example.com"; // Replace with recipient's email address
+    var toAddress = "some@example.com"; 
 
-    // Create a new SmtpClient
+
     using (var smtpClient = new SmtpClient(smtpHost, smtpPort))
     {
-        smtpClient.EnableSsl = true; // Use TLS encryption
+        smtpClient.EnableSsl = true; 
         smtpClient.Credentials = new NetworkCredential(smtpUser, smtpPassword);
 
-        // Create the email message
         var mailMessage = new MailMessage(fromAddress, toAddress)
         {
             Subject = "Test Email",
@@ -31,10 +29,14 @@ app.MapGet("/send-test-email", async context =>
             IsBodyHtml = true
         };
 
-        // Add custom headers
+        mailMessage.CC.Add("test.one@example.com");
+        mailMessage.CC.Add("test.two@example.com");       
+
+        // mailMessage.Bcc.Add("test.one@example.com");
+        // mailMessage.Bcc.Add("test.two@example.com");
+
         mailMessage.Headers.Add("x-liara-tag", "test-tag");
 
-        // Send the email
         try
         {
             await smtpClient.SendMailAsync(mailMessage);
